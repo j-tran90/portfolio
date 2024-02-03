1e7ed48bb9e2578f86f547923ebec0f1eea0726b
j-tran90 <johnt90@live.com>
2024-02-03 11:25:24 -0500

    changed card style

--- src/components/Projectcards.jsx
+++ src/components/Projectcards.jsx
@@ -1,90 +1,62 @@
 import "../stylesheets/Projectcards.css";
-import { Col } from "react-bootstrap";
 
 function ProjectCards() {
   return (
     <>
-      <div className="grid container-fluid pb-5">
+      <div className="container-fluid pb-5">
         <div className="container p-5 display-1 text-center">Portfolio</div>
-        <div className="row row-cols-md-2">
-          <a className="nav-link" href="/couriertek">
-            <Col>
-              <div id="card-1">
-                {/* <img
-                  src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/portfolio%2Fvecteezy_abstract-orange-fluid-wave-background_.jpg?alt=media&token=bb03a23c-6e9d-4b82-b9b9-123f58c353c4"
-                  alt=""
-                  loading="eager"
-                /> */}
-                <h2>
-                  Freelance <span>CourierTek</span>
-                </h2>
-                <p>
-                  A courier tracking solution for business all types and sizes.
-                </p>
-              </div>
-            </Col>
-          </a>
 
-          <a className="nav-link" href="/seashell">
-            <div className="col-sm">
-              <figure className="">
-                <img
-                  src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/portfolio%2Fvecteezy_abstract-blue-gradient-fluid-background_6736876.jpg?alt=media&token=b9c03862-0d66-4c04-8fbd-9305742ccad9"
-                  alt=""
-                  loading="eager"
-                />
-                <figcaption className="figtext">
-                  <h2>
-                    Full Stack Project <span>SeaShell</span>
-                  </h2>
-                  <p>
-                    A light chat application. A solo full stack project using
-                    React, Javascript, Boostrap, and Firebase
-                  </p>
-                </figcaption>
-              </figure>
+        <div className="card-group">
+          <div className="card">
+            <img
+              className="card-img-top"
+              src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/portfolio%2Fvecteezy_abstract-orange-fluid-wave-background_.jpg?alt=media&token=bb03a23c-6e9d-4b82-b9b9-123f58c353c4"
+              alt="Card image cap"
+            />
+            <div className="card-body">
+              <h1 className="display-4 card-title">CourierTek</h1>
+              <p className="card-text">
+                A Free lance project. A courier tracking solution for business
+                all types and sizes.
+              </p>
+              <a className="btn btn-warning" href="/couriertek" role="button">
+                See More
+              </a>
             </div>
-          </a>
-
-          <a className="nav-link" href="/nhoracing">
-            <div className="col">
-              <figure className="effect-oscar">
-                <a href="/nhoracing">
-                  <img
-                    src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/portfolio%2Fvecteezy_abstract-red-fluid-wave-background_.jpg?alt=media&token=dabfb2f6-3770-4e7e-9f84-b27e4d5fdda1"
-                    alt="img09"
-                    loading="eager"
-                  />
-                  <figcaption>
-                    <h2>
-                      Freelance <span>Nho Racing</span>
-                    </h2>
-                    <p>
-                      A freelance project taking on client's request to create a
-                      simple static webpage to showcase an imported vehicle.
-                    </p>
-                  </figcaption>
-                </a>
-              </figure>
+          </div>
+          <div className="card">
+            <img
+              className="card-img-top"
+              src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/portfolio%2Fvecteezy_abstract-blue-gradient-fluid-background_6736876.jpg?alt=media&token=b9c03862-0d66-4c04-8fbd-9305742ccad9"
+              alt="Card image cap"
+            />
+            <div className="card-body">
+              <h5 className="display-4 card-title">SeaShell</h5>
+              <p className="card-text">
+                A light chat application. A solo full stack project using React,
+                Javascript, Boostrap, and Firebase
+              </p>
+              <a className="btn btn-info" href="/seashell" role="button">
+                See More
+              </a>
             </div>
-          </a>
-          <div className="col">
-            <figure className="effect-oscar">
-              <img
-                src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/portfolio%2Fvecteezy_abstract-blue-fluid-wave-background_.jpg?alt=media&token=7b3ac57a-be52-4a3b-9b59-87dcd329a634"
-                alt="img09"
-                loading="lazy"
-              />
-              <figcaption>
-                <h2>
-                  Full Stack Group Project <span>Tix</span>
-                </h2>
-                <p>
-                  A survey generator. A full stack group project using Express,
-                  Typescript, Bootstrap, and MongoDB.
-                </p>
-              </figcaption>
-            </figure>
+          </div>
+          <div className="card">
+            <img
+              className="card-img-top"
+              src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/portfolio%2Fvecteezy_abstract-red-fluid-wave-background_.jpg?alt=media&token=dabfb2f6-3770-4e7e-9f84-b27e4d5fdda1"
+              alt="Card image cap"
+            />
+            <div className="card-body">
+              <h5 className="display-4 card-title">Nho Racing</h5>
+              <p className="card-text">
+                A freelance project taking on client's request to create a
+                simple static webpage to showcase an imported vehicle.
+              </p>
+              <a className="btn btn-danger" href="/nhoracing" role="button">
+                See More
+              </a>
+            </div>
           </div>
         </div>
       </div>
