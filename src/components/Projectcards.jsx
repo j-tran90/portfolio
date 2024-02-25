93856e27f4f09e6aae95dc3854227b3aee126a5b
j-tran90 <johnt90@live.com>
2024-02-25 18:03:54 -0500

    minor project card change

--- src/components/Projectcards.jsx
+++ src/components/Projectcards.jsx
@@ -1,4 +1,5 @@
 import "../stylesheets/Projectcards.css";
+import "../stylesheets/Skills.css";
 
 function ProjectCards() {
   return (
@@ -9,12 +10,14 @@ function ProjectCards() {
         </div>
 
         <div className="card-group">
-          <div className="card">
-            <img
-              className="card-img-top card-1 crop-img"
-              src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/portfolio%2Fct-2.png?alt=media&token=0b7aa052-0d57-41f6-a181-67d4f1814894"
-              alt="Card image cap"
-            />
+          <div className="card brighten">
+            <a href="https://glamorous-tan-slacks.cyclic.app/">
+              <img
+                className="card-img-top card-1 crop-img "
+                src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/portfolio%2Fct-2.png?alt=media&token=0b7aa052-0d57-41f6-a181-67d4f1814894"
+                alt="Card image cap"
+              />
+            </a>
             <div className="card-body">
               <div className="display-4 card-title">CourierTek</div>
               <p className="card-text">
@@ -37,12 +40,14 @@ function ProjectCards() {
               </a>
             </div>
           </div>
-          <div className="card">
-            <img
-              className="card-img-top card-1 crop-img"
-              src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/portfolio%2Fseashell.png?alt=media&token=3dcb7ed7-d0b3-4632-bbc4-b1022bc6f87a"
-              alt="Card image cap"
-            />
+          <div className="card brighten">
+            <a href="https://seashell.vercel.app/">
+              <img
+                className="card-img-top card-1 crop-img"
+                src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/portfolio%2Fseashell.png?alt=media&token=3dcb7ed7-d0b3-4632-bbc4-b1022bc6f87a"
+                alt="Card image cap"
+              />
+            </a>
             <div className="card-body">
               <div className="display-4 card-title">SeaShell</div>
               <p className="card-text">
@@ -57,20 +62,22 @@ function ProjectCards() {
                 Tech Stack
               </a>
               <a
-                className="btn btn-info mx-2"
                 href="https://seashell.vercel.app/"
+                className="btn btn-info mx-2"
                 role="button"
               >
                 Go to SeaShell
               </a>
             </div>
           </div>
-          <div className="card">
-            <img
-              className="card-img-top card-1 crop-img"
-              src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/portfolio%2Fnhoracing.png?alt=media&token=a3a312c0-3019-4aa8-9bc6-c3cc065e5292"
-              alt="Card image cap"
-            />
+          <div className="card brighten">
+            <a href="https://nhoracing.vercel.app/">
+              <img
+                className="card-img-top card-1 crop-img"
+                src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/portfolio%2Fnhoracing.png?alt=media&token=a3a312c0-3019-4aa8-9bc6-c3cc065e5292"
+                alt="nho racing"
+              />
+            </a>
             <div className="card-body">
               <div className="display-4 card-title">Nho Racing</div>
               <p className="card-text">
@@ -93,12 +100,15 @@ function ProjectCards() {
               </a>
             </div>
           </div>
-          <div className="card">
-            <img
-              className="card-img-top card-1 crop-img"
-              src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/portfolio%2Fcalorator.png?alt=media&token=bd3dee5a-dadd-4638-9115-33deff977b71"
-              alt="Card image cap"
-            />
+
+          <div className="card brighten">
+            <a href="https://calorator.vercel.app/">
+              <img
+                className="card-img-top card-1 crop-img"
+                src="https://firebasestorage.googleapis.com/v0/b/auth-development-ae591.appspot.com/o/portfolio%2Fcalorator.png?alt=media&token=bd3dee5a-dadd-4638-9115-33deff977b71"
+                alt="calorator"
+              />
+            </a>
             <div className="card-body">
               <div className="display-4 card-title">Calorator</div>
               <p className="card-text">
